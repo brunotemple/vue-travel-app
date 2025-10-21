e0101f6cd95c6556cafcd4bbd9f58cc83cfe9ca0
brunotemple <bruno.temple@gmail.com>
2025-10-21 14:20:17 -0400

    better auth

---
+++ components/auth-button.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { createAuthClient } from "better-auth/client";
+
+const authClient = createAuthClient();
+
+async function signIn() {
+  await authClient.signIn.social({ provider: "github", callbackURL: "/dashboard" });
+}
+</script>
+
+<template>
+  <button class="btn btn-accent" @click="signIn">
+    Sign In with Github
+    <Icon name="tabler:brand-github" size="24" />
+  </button>
+</template>
